5664b59a77b9b7bfe85295c51980c02a3b424536
Guilherme <98982687+yFariiias@users.noreply.github.com>
2025-02-22 21:17:57 -0300

    Add files via upload

---
+++ js/animações.js
@@ -0,0 +1,90 @@
+// Animação do header
+
+document.addEventListener('DOMContentLoaded', () => {
+    const header = document.querySelector('header');
+    let lastScroll = 0;
+    const threshold = 80;
+
+    function handleScroll() {
+        const currentScroll = window.scrollY;
+
+        if (currentScroll <= threshold) {
+
+            header.classList.remove('hide');
+            header.classList.add('show');
+        } else if (currentScroll < lastScroll) {
+
+            if (currentScroll <= threshold) {
+
+                header.classList.remove('hide');
+                header.classList.add('show');
+            } else {
+
+                header.classList.remove('show');
+                header.classList.add('hide');
+            }
+        } else if (currentScroll > lastScroll) {
+
+            header.classList.remove('show');
+            header.classList.add('hide');
+        }
+
+        lastScroll = currentScroll;
+    }
+
+    window.addEventListener('scroll', handleScroll);
+
+    handleScroll();
+});
+
+// Animação do footer
+
+document.addEventListener("DOMContentLoaded", function () {
+    const footer = document.querySelector(".footer");
+
+    function checkScroll() {
+        const scrollPosition = window.scrollY + window.innerHeight;
+        const footerPosition = footer.offsetTop;
+
+        
+        if (scrollPosition > footerPosition - 100) {
+
+            footer.style.opacity = "1";
+            footer.style.transform = "translateY(0)";
+        } else {
+
+            footer.style.opacity = "0";
+            footer.style.transform = "translateY(20px)";
+        }
+    }
+
+    window.addEventListener("scroll", checkScroll);
+    checkScroll();
+});
+
+// Proibição de arrastar elementos
+
+document.addEventListener('dragstart', function(e) {
+    e.preventDefault(); //Para ativar remova a palavra "desativado"
+});
+
+
+// Animação dos elementos Projetos e Main-content
+document.addEventListener("DOMContentLoaded", function () {
+    const elementsToAnimate = document.querySelectorAll(".hidden");
+
+    function checkScroll() {
+        elementsToAnimate.forEach((element) => {
+            const elementTop = element.getBoundingClientRect().top;
+            const windowHeight = window.innerHeight;
+
+            if (elementTop < windowHeight - 100) {
+                element.classList.add("fade-in");
+                element.classList.remove("hidden");
+            }
+        });
+    }
+
+    window.addEventListener("scroll", checkScroll);
+    checkScroll();
+});
